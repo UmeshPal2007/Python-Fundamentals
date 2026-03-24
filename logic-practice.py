6e53f9c0911013e5e1295c154dc9bdbd2b9cab00
Umesh Pal <umeshpal.github@gmail.com>
2026-03-24 16:04:19 +0530

    Create logic-practice.py

---
+++ logic-practice.py
@@ -0,0 +1,9 @@
+#First Program
+name = input("Enter Your Name : ")
+age = int(input("Enter Your Age : "))
+marks = float(input("Enter Your Marks : "))
+
+print("Your Name is : ", name)
+print("Your Age is : ", age)
+print("Your Marks are : ", marks)
+
